f245f5551da53519146560811cd73a65ebb46bfc
ici.be <bruno.veyckemans@gmail.com>
2020-03-11 14:44:02 +0100

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -3,7 +3,7 @@ class Main
 {
   const GEOSERVER_URBIS_ADM   = "//geoservices-urbis.irisnet.be/geoserver/UrbisAdm/wms";
   const GEOSERVER_URBIS_LOC   = "//geoservices.irisnet.be/localization/Rest/Localize";
-  const GEOSERVER_URBIS     = "http://geoservices-others.irisnet.be/geoserver/ows";
+  const GEOSERVER_URBIS     = "//geoservices-others.irisnet.be/geoserver/ows";
   const GEOSERVER_BRUGIS      = "//www.mybrugis.irisnet.be/geoserver/wms";
   const GEOSERVER_BRU_MOBIL   = "//data-mobility.brussels/geoserver/bm_public_transport/wms";
   const TIMEOUT = 10; 
