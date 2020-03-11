1137b07293e747ff0fb80aa93e16608f6f51c07b
ici.be <bruno.veyckemans@gmail.com>
2020-03-11 13:47:30 +0100

    Update and rename NovaPermits.php to BlablaPermits.php

--- src/BlablaPermits.php
+++ src/BlablaPermits.php
@@ -1,5 +1,5 @@
 <?php
-class NovaPermits
+class BlablaPermits
 {
   const GEOSERVER_URBIS_ADM   = "//geoservices-urbis.irisnet.be/geoserver/UrbisAdm/wms";
   const GEOSERVER_URBIS_LOC   = "//geoservices.irisnet.be/localization/Rest/Localize";
