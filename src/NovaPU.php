28b3e430411b8df53baa5a8df7465ccf5d4e7b02
ici.be <bruno.veyckemans@gmail.com>
2020-03-11 14:11:57 +0100

    Update NovaPU.php

--- src/NovaPU.php
+++ src/NovaPU.php
@@ -1,13 +1,6 @@
 <?php
-class NovaPU
+class NovaPU extends Main
 {
-  const GEOSERVER_URBIS_ADM   = "//geoservices-urbis.irisnet.be/geoserver/UrbisAdm/wms";
-  const GEOSERVER_URBIS_LOC   = "//geoservices.irisnet.be/localization/Rest/Localize";
-  const GEOSERVER_URBIS       = "//geoservices-others.irisnet.be/geoserver/ows";
-  const GEOSERVER_BRUGIS      = "//www.mybrugis.irisnet.be/geoserver/wms";
-  const GEOSERVER_BRU_MOBIL   = "//data-mobility.brussels/geoserver/bm_public_transport/wms";
-  const TIMEOUT = 10; 
-
   public static function getPermitType(string $refnova)
   {
     if( strstr($refnova, 'IPE') || strstr($refnova, 'CL') || strstr($refnova, 'ICE') || strstr($refnova, 'RC_MOD') ) {
