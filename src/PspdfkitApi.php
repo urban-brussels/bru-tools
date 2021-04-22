01ee798f1452bbc94375fd7dfd3bb33108ac4792
ici.be <bruno.veyckemans@gmail.com>
2021-04-22 09:10:05 +0200

    Create PspdfkitApi.php

---
+++ src/PspdfkitApi.php
@@ -0,0 +1,29 @@
+<?php
+
+use Symfony\Component\HttpClient\HttpClient;
+
+class PspdfkitApi
+{
+
+    public static function uploadDocument(string $uuid, string $path)
+    {
+        $curl = "curl -X POST \"http://pvdcl-arndr01:5000/api/documents\" -H  \"accept: application/json\" -H  \"Authorization: Token token=secret\" -H  \"Content-Type: multipart/form-data\" -F \"document_id=".$uuid."\" -F \"keep_current_annotations=\" -F \"overwrite_existing_document=false\" -F \"title=\" -F \"url=\" -F \"file=@".$path.".$uuid."\"";
+        exec($curl);
+    }
+
+    public static function checkDocument(string $uuid)
+    {
+        $httpClient = HttpClient::create();
+        $response = $httpClient->request(
+            'GET',
+            "http://pvdcl-arndr01:5000/api/documents/".$uuid."/document_info",
+            [
+                'headers' => [
+                    'Authorization' => 'Token token=secret',
+                ],
+            ]
+        );
+
+        return $response->getStatusCode() === 200 ? true : false;
+    }
+}
